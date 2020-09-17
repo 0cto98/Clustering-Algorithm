0bea98cc5a160102cd00b001de6f7ceb8754d576
Tom RIVERO <tom.rivero60330@gmail.com>
2020-09-17 09:33:37 +0200

    Updated lenght method to handle more than 2 points

--- test_TDD_trajectorie.py
+++ test_TDD_trajectorie.py
@@ -13,9 +13,10 @@ class Trajectorie:
     def lenght(self):
         l = 0
         if len(self.points)>=2:
-            p0 = self.points[0]
-            p1 = self.points[1]
-            l = sqrt( ((p1.x-p0.x)**2)+((p1.y-p0.y)**2) )
+            for i in range (len(self.points)-1):
+                p0 = self.points[i]
+                p1 = self.points[i+1]
+                l += sqrt( ((p1.x-p0.x)**2)+((p1.y-p0.y)**2) )
         return l
 
 
@@ -42,3 +43,13 @@ def test_traj_lenght_1point():
     p1=Point(0,0)
     t.add_point(p1)
     assert t.lenght()==0
+
+def test_traj_lenght_gt2point():
+    t=Trajectorie()
+    p1=Point(0,0)
+    p2=Point(1,0)
+    p3=Point(1,4)
+    t.add_point(p1)
+    t.add_point(p2)
+    t.add_point(p3)
+    assert t.lenght()==5
