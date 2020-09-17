160bb96ee08870afbe2c95685b04882f5d28dd65
Tom RIVERO <tom.rivero60330@gmail.com>
2020-09-17 09:23:48 +0200

    Added trajectorie lenght method for 2 points traj

--- test_TDD_trajectorie.py
+++ test_TDD_trajectorie.py
@@ -10,6 +10,12 @@ class Trajectorie:
     def add_point(self,P):
         self.points.append(P)
 
+    def lenght(self):
+        p0 = self.points[0]
+        p1 = self.points[1]
+        l = sqrt( ((p1.x-p0.x)**2)+((p1.y-p0.y)**2) )
+        return l
+
 
 def test_traj_init():
     t = Trajectorie()
@@ -20,3 +26,11 @@ def test_add_point():
     p1=Point(0,0)
     t.add_point(p1)
     assert p1 in t.points
+
+def test_traj_lenght_2points():
+    t=Trajectorie()
+    p1=Point(0,0)
+    p2=Point(1,0)
+    t.add_point(p1)
+    t.add_point(p2)
+    assert t.lenght()==1
