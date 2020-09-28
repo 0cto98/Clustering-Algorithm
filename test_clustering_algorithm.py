4fb7e87473a401997334464ed10a01b7ebf52325
Tom Rivero <tom.rivero60330@gmail.com>
2020-09-28 12:18:36 +0200

    work in progress, but i commit it just in case

--- test_clustering_algorithm.py
+++ test_clustering_algorithm.py
@@ -1,20 +1,30 @@
 from test_TDD_trajectorie import *
 from graph import *
 
-def clustering_algorith(traj_list):
-    g= Graph()
+
+#Take the list of trajectories A and return an other list of trajectories B so that every trajectorie which cross a square of the grid as a point in this square
+def grid_traj(traj_list, grid_size):    
+    grid_traj_list = []
     for traj in traj_list:
-        for i in range (len(traj.points)-1):
-            g.addEdge(traj.point_i(i),(traj.point_i(i+1)))
-    return g
+        t = Trajectorie()
+        for i in range(traj.number_of_points()-1):
+            p1=traj.point_i(i)
+            t.add_point(p1)
+            p2=traj.point_i(i+1)
+            if(p1.distance(p2)>grid_size):
+                pass
+                
 
-
-def test_single_traj():
+"""
+def test_grid_single_traj():
     t=Trajectorie()
     t.add_point(Point(0,0))
     t.add_point(Point(1,0))
 
-    g= Graph()
-    g.addEdge(Point(0,0), Point(1,0))
+    t2=Trajectorie()
+    t2.add_point(Point(0,0))
+    t2.add_point(Point(0.5,0.5))
+    t2.add_point(Point(1,1))
 
-    assert clustering_algorith([t]) == g
+    assert grid_traj([t],0.5) == [t2]
+"""
