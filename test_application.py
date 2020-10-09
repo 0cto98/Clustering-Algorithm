11881629bf2fa6c204d9574b4090d1f34796f670
Tom Rivero <tom.rivero60330@gmail.com>
2020-10-09 10:00:52 +0200

    pushed everything because idk anymore what i changed recently

--- test_application.py
+++ test_application.py
@@ -1,20 +1,45 @@
 from test_clustering_algorithm import *
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class Application:
     def __init__(self, path, grid_size):
         self.traj_list = import_data(path)
         self.grid_traj_list = grid_traj(self.traj_list, grid_size)
-        self.centered_grid_traj_list = center_points_grid_cells(self.grid_traj_list, grid_size*2)
+        self.centered_grid_traj_list = center_points_grid_cells(self.grid_traj_list, grid_size)
         self.graph = centered_grid_traj_list_to_graph(self.centered_grid_traj_list)
-        pickle.dump(self.graph, open("graph", "wb"))
+        pickle.dump(self.graph, open(str(dir_path)+"/graph", "wb"))
+        self.show_base_traj()
+        self.show_crustered_traj()
+        plt.show()
 
-app = Application("C:/Users/tomri/Desktop/Master 1/Clustering-Algorithm/cabspottingdata",0.1)
-g = pickle.load(open("graph", "rb"))
-for edge in g.edges:
-    x = [edge[0].x, edge[1].x]
-    y = [edge[0].y, edge[1].y]
-    plt.plot(x, y, c='r')
-plt.savefig('Cluster.png') #Save the plot as .png file
-plt.show()
+    def show_base_traj(self):
+        plt.figure(1)
+        for traj in self.traj_list:
+            l = int(traj.number_of_points())
+            x, y = [], []
+            for i in range (l):
+                p = traj.point_i(i)
+                x.append(p.x)
+                y.append(p.y)
+            plt.plot(x,y)
+        plt.axis('equal')
+        plt.savefig(str(dir_path)+'/Base_Trajecories.png') #Save the plot as .png file
+        plt.grid(True)
+
+    def show_crustered_traj(self):
+        plt.figure(2)
+        g = pickle.load(open(str(dir_path)+"/graph", "rb"))
+        for edge in g.edges:
+            x = [edge[0].x, edge[1].x]
+            y = [edge[0].y, edge[1].y]
+            plt.plot(x, y, c='r')
+        plt.axis('equal')
+        plt.savefig(str(dir_path)+'/Cluster.png') #Save the plot as .png file
+        plt.grid(True)
+
+app = Application(str(dir_path)+"/cabspottingdata",0.02)
+
--- test_point.py
+++ test_point.py
@@ -1,17 +1,20 @@
-from math import sqrt
+import numpy as np
 
 class Point:
+    #Initialize the Point with coordinates (x,y)
     def __init__(self, x ,y):
         self.x = x
         self.y = y
 
+    #Print the Point(usefull for debugging)
     def __str__(self):
         return '('+str(self.x)+','+str(self.y)+')'
 
+    #Return the distance between two Points
     def distance(self, other):
         dx = self.x - other.x
         dy = self.y - other.y
-        return sqrt(dx * dx + dy * dy)
+        return np.sqrt(dx * dx + dy * dy)
 
     def __eq__(self,other):
         return (self.x == other.x and self.y == other.y)
@@ -19,6 +22,7 @@ class Point:
     def __hash__(self):
         return hash((self.x, self.y))
 
+    #Return the middle Point between two Points
     def middle(self, other):
         return Point((self.x+other.x)/2,(self.y+other.y)/2)
 
@@ -31,7 +35,7 @@ def test_init():
 def test_distance():
     p1 = Point(1,1)
     p2 = Point(0,0)
-    assert p1.distance(p2) == sqrt(2)
+    assert p1.distance(p2) == np.sqrt(2)
 
 def test_eq():
     p1 = Point(1,1)
